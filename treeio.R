d44ae26b495584c5b989f5cddfdd417c35c39533
omicsanalysis <omicsanalysismx@gmail.com>
2025-01-20 19:48:22 -0600

    Added `treeio` tutorial

---
+++ treeio.R
@@ -0,0 +1,68 @@
+# https://www.r-bloggers.com/2018/05/treeio-phylogenetic-data-integration
+
+# basic loadup
+library(treeio)
+library(tidyverse)
+tree0=read.newick("example_phylo.nwk")
+class(tree0) # simple phylo tree
+print(tree0) # simple summary
+
+# load metadata
+Design=read.table("Experiment_Design_example_1.tsv",sep="\t",header=T)
+colnames(Design)
+rownames(Design)=Design$Assembly.Accession
+
+# subset metadata
+chrs=c("Strain")
+fcts=c("Organism","Host","Assembly_level")
+nums=c("Count_gene_total","ANI","BUSCO_Scaffold_N50_.MB.","BUSCO_Contigs_N50_.MB.","CheckM_Completeness")
+
+x=data.frame(label=tree0$tip.label)
+for(i in fcts) {
+    x[,i]=rep("",length(x$label))
+    for(n in x$label){
+        x[which(x$label==n),i]=as.character(Design[n,i])
+    }
+    x[,i]=as_factor(x[,i])
+}
+for(i in chrs) {
+    x[,i]=rep("",length(x$label))
+    for(n in x$label){
+        x[which(x$label==n),i]=as.character(Design[n,i])
+    }
+}
+for(i in nums) {
+    x[,i]=rep(0,length(x$label))
+    for(n in x$label){
+        x[which(x$label==n),i]=Design[n,i]
+    }
+    x[,i]=as.numeric(x[,i])
+}
+
+# merge metadata into tree
+tree1=full_join(tree0,x,by="label")
+class(tree1) # treedata with attributes (the table)
+print(tree1) # summary + tibble with data
+
+# plot tree
+library(ggtree)
+library(RColorBrewer)
+
+wd=20
+ht=40
+
+plo=ggtree(tree1) +
+    geom_nodelab(size=ht*0.05,color="black",breaks=NA) +
+    geom_tiplab(aes(label=label),size=ht*0.05,fontface=2) +
+    geom_tippoint(aes(color=Organism)) +
+    scale_color_manual("Organism",
+                       values=brewer.pal(length(levels(x$Organism)),"Paired")
+                      ) +
+    theme(legend.title=element_text(size=ht*0.3),
+          legend.text=element_text(size=ht*0.25),
+          legend.position="bottom")
+
+pdf("example_phylo_w_orgs.pdf",width=wd,height=ht)
+print(plo)
+dev.off()
+
