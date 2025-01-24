a46c90fb15b676b46d91014fe91bf1d2426eb42e
ecuevasomics <e.cuevas.oan@gmail.com>
2025-01-23 19:32:53 -0600

    Other plot version with Assembly Level metadata

--- Cmi_plot.R
+++ Cmi_plot.R
@@ -25,15 +25,21 @@ metadata_ordered <- metadata_ordered %>%
 # readr::write_tsv(metadata_ordered, "metada_ordered_example1.tsv")
 metadata_ordered <- readr::read_tsv("metada_ordered_example1.tsv")
 
+# tree$tip.label <- metadata_ordered$Strain
+
+
 #Convert tree information to tibble object and add metadata
 treetib<-as_tibble(tree)
-treetib<-left_join(treetib, metadata_ordered, by = 'label')
+treetib<-left_join(treetib, metadata_ordered, by = c('label'))
 
 tree_cm <- as.treedata(treetib)
 
+tree_cm
+
+# State plot
 
 p <- ggtree(tree_cm, ladderize = TRUE, layout = "circular") + 
-  geom_tiplab(aes(label = label, angle = angle),
+  geom_tiplab(aes(label = Strain, angle = angle),
               linesize = .07, size = 5, align = TRUE, offset = 0.001) +
   geom_tippoint(aes(color = Country), size = 5, shape = 20) +
   geom_tippoint(aes(shape = Source), size = 5, alpha = 0.5) +
@@ -43,7 +49,9 @@ p <- ggtree(tree_cm, ladderize = TRUE, layout = "circular") +
                                 "#a5a436", "#36a540", "#365ba5", "#0cf3f3",
                                 "#0cf31e", "#e87c09", "#e80917", "#f81df1",
                                 "darkblue", "darkgrey", "#1df83b", "#d65c40",
-                                "#40d689", "grey"))
+                                "#40d689", "grey", "brown", "green", "pink",
+                                "orange"))
+
 
 matrix_heatmap <- data.frame(metadata_ordered %>% 
                                select(Estado))
@@ -63,4 +71,42 @@ gheatmap(p, matrix_heatmap, width=0.05,
                                 "darkblue", "darkgrey", 
                                 "#40d689", "grey")) +
   labs(fill = "Mexican State")
+
+
+
+# Assembly Level plot
+
+p <- ggtree(tree_cm, ladderize = TRUE, layout = "circular") + 
+  geom_tiplab(aes(label = Strain, angle = angle),
+              linesize = .07, size = 5, align = TRUE, offset = 0.001) +
+  geom_tippoint(aes(color = Estado), size = 5, shape = 20) +
+  geom_tippoint(aes(shape = Source), size = 5, alpha = 0.5) +
+  scale_shape_manual(values = c(18, 1, 18)) +
+  labs(shape = "Source")+ geom_rootpoint(col="red") +
+  scale_color_manual(values = c("#375c5d", "#b360a3", "darkred", "darkcyan",
+                                "#a5a436", "#36a540", "#365ba5", "#0cf3f3",
+                                "#0cf31e", "#e87c09", "#e80917", "#f81df1",
+                                "darkblue", "darkgrey", "#1df83b", "#d65c40",
+                                "#40d689", "grey", "brown", "green", "pink",
+                                "orange"))
+
+
+matrix_heatmap <- data.frame(metadata_ordered %>% 
+                               select(Assembly_level))
+
+
+rownames(matrix_heatmap) <- make.unique(metadata_ordered$label)
+
+# Add heatmap
+gheatmap(p, matrix_heatmap, width=0.05, 
+         colnames=FALSE, legend_title="State", color = NA, ) +
+  theme(text = element_text(size = 20),
+        legend.position = c(1.14,0.5), 
+        plot.margin = margin(t = 50, r = 10, b = 50, l = 10)) +
+  scale_fill_manual(values = c("darkred",
+                               "#f81df1", "#e87c09", "#0cf3f3",
+                               "#0cf31e", 
+                               "darkblue", "darkgrey", 
+                               "#40d689", "grey")) +
+  labs(fill = "Assembly Level")
 
